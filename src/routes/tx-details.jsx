706512eb738bf290a256cbdd8989b94a579b8060
Maximo Gijon <maximo.gijon@gmail.com>
2022-12-23 18:33:16 -0300

    implement tx details

--- src/routes/tx-details.jsx
+++ src/routes/tx-details.jsx
@@ -1,3 +1,35 @@
+import { Utils } from "alchemy-sdk";
+import { Link, useLoaderData } from "react-router-dom";
+import { alchemy } from "./root";
+
+export async function loader({ params }) {
+  const transaction = await alchemy.core.getTransaction(params.txHash);
+  return { transaction };
+}
+
 export default function TxDetails() {
-  return <div>Tx Details</div>;
+  const { transaction } = useLoaderData();
+  console.log(transaction);
+
+  return (
+    <div>
+      <div>Transaction: {transaction.hash}</div>
+      <div>
+        Block:
+        <Link to={`/block/${transaction.blockNumber}`}>{transaction.blockNumber}</Link>
+      </div>
+      <div>
+        From:
+        <Link to={`/address/${transaction.from}`}>{transaction.from}</Link>
+      </div>
+      <div>
+        To:
+        <Link to={`/address/${transaction.to}`}>{transaction.to}</Link>
+      </div>
+      <div>Confirmations: {transaction.confirmations}</div>
+      <div>Value: {Utils.formatUnits(transaction.value)} ETH</div>
+      <div>Gas Price: {Utils.formatUnits(transaction.gasPrice)} ETH </div>
+      <div>Gas Limit: {Utils.formatUnits(transaction.gasLimit) * 10 ** 18} </div>
+    </div>
+  );
 }
